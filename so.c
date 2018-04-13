00fe88a27f720c88cac0245f7cd40515b4291ffb
José Victor Viriato <jvviriato@inf.ufsm.br>
2018-04-13 14:20:16 -0300

    temos nosso proprio tempooo

--- so.c
+++ so.c
@@ -7,134 +7,141 @@
 #include <time.h>
 
 
-#define ROOT getpid()
+void gera_arvore_branch(int nivel, int altura, int root)    
+{
+    pid_t ArvEsq, ArvDir;
 
-void gera_arvore_branch(int nivel, int altura, int pid_root);
-void gera_arvore_livre(int nivel, int altura);
+    if(nivel == 0){
+        printf("------------- Branch -------------\n");
+        printf("n = %d      PID=%d (root)\n", nivel, getpid());
+    }
+
+    nivel++;
+
+    if (nivel > altura) 
+        return;
+
+    ArvEsq = fork();
+
+    if (ArvEsq < 0){   
+        fprintf(stderr, "fork falhou\n");
+        exit(-1);
+    }
+    
+    else if (ArvEsq == 0){  
+        printf("n = %d      C[%d, %d]\n", nivel, getpid(), getppid());
+        gera_arvore_branch(nivel, altura, root);
+        printf("           T[%d, %d]\n", getpid(), getppid());
+        exit(0);
+    }
+    
+    else{     
+        wait(&ArvEsq);
+        ArvDir = fork();
+
+    if (ArvDir < 0){    
+        fprintf(stderr, "fork falhou\n");
+        exit(-1);
+    }
+    else if (ArvDir == 0){   
+        printf("n = %d      C[%d, %d]\n", nivel, getpid(), getppid());
+        gera_arvore_branch(nivel, altura, root);
+        printf("           T[%d, %d]\n", getpid(), getppid());
+        exit(0);
+    }
+        wait(&ArvDir);
+    }
+    if(getpid() == root)
+        printf("--- Fim criacao por Branch --\n");
+
+    return;
+}
+
+void gera_arvore_livre(int nivel, int altura, int root)    
+{
+    pid_t ArvEsq, ArvDir;
+
+    if(nivel == 0){
+        printf("\n------------- Livre -------------\n");
+        printf("n = %d      PID=%d (root)\n", nivel, getpid());
+    }
+
+    nivel++;
+
+    if (nivel > altura) 
+        return;
+
+    ArvEsq = fork();
+
+    if (ArvEsq < 0){   
+        fprintf(stderr, "fork falhou\n");
+        exit(-1);
+    }
+    
+    else if (ArvEsq == 0){  
+        printf("n = %d      C[%d, %d]\n", nivel, getpid(), getppid());
+        gera_arvore_livre(nivel, altura, root);
+        printf("           T[%d, %d]\n", getpid(), getppid());
+        exit(0);
+    }
+    
+    else{     
+        ArvDir = fork();
+
+    if (ArvDir < 0){    
+        fprintf(stderr, "fork falhou\n");
+        exit(-1);
+    }
+    else if (ArvDir == 0){   
+        printf("n = %d      C[%d, %d]\n", nivel, getpid(), getppid());
+        gera_arvore_livre(nivel, altura, root);
+        printf("           T[%d, %d]\n", getpid(), getppid());
+        exit(0);
+    }
+        wait(&ArvEsq);
+        wait(&ArvDir);
+    }
+    if(getpid() == root)
+        printf("--- Fim criacao por Livre --\n");
+
+    return;
+}
+
 
 int main(int argc, char **argv)
 {
-    if (argc == 2 && atoi(argv[1]) > 0) {
-        int altura = atoi(argv[1]);
+    int altura = atoi(argv[1]);
+
+    if (argc == 2 && altura > 1) {
         printf("A altura da árvore é de: %d\n", altura);
-
         double num_Processos = pow(2, altura+1) - 1;
-        printf("Serão criados %lf processos.\n\n", num_Processos);
+        printf("Serão criados %.lf processos.\n\n", num_Processos);
         
+        pid_t root = getpid();
 
-        pid_t id = getpid();
 
-        clock_t inicio_tempo_Branch = clock();
-        gera_arvore_branch(0, altura, ROOT);
+        clock_t t0_branch = clock();
+        gera_arvore_branch(0, altura, getpid());
+        clock_t t1_branch = clock();
+        double tempo_branch = (t1_branch-t0_branch)*1000.0 / CLOCKS_PER_SEC;
 
-        // double duracao = (fim_tempo_Branch - inicio_tempo_Branch)*1000.0 / CLOCKS_PER_SEC;
-        // printf("Tempo de execucao da Arvore: %f s\n", duracao);
+        clock_t t0_livre = clock();
+        gera_arvore_livre(0, altura, getpid());
+        clock_t t1_livre = clock();
+        double tempo_livre = (t1_livre-t0_livre)*1000.0 / CLOCKS_PER_SEC;
+   
 
-        if(getpid() == id){
-            clock_t fim_tempo_Branch = clock();
-            double duracao = (fim_tempo_Branch - inicio_tempo_Branch)*1000.0 / CLOCKS_PER_SEC;
-		    printf("Tempo de execucao da Arvore: %f s\n", duracao);
+        if(getpid() == root){
+            
+            printf ("Tempo execucao 'Branch': %g ms\n", tempo_branch);
+            printf ("Tempo execucao 'Livre': %g ms\n", tempo_livre);
+
         }
-        // gera_arvore_livre(0, altura);
-
-
     }
     else {
         printf("Entrada inválida\n");
     }
+    printf("Fim PID = %d (root)\n", getpid());
+
     return 0;
 }
-
-void gera_arvore_branch(int nivel, int altura, int pid_root)
-{
-    pid_t ArvEsq, ArvDir;
-
-    if (nivel == 0){
-        printf("--------------- Branch ---------------\n");
-        printf("n = 0        PID: %d (root)\n", getpid());
-    }
-    nivel++;
-    if (nivel <= altura){
-        ArvEsq = fork();
-        if (ArvEsq < 0){
-			fprintf(stderr, "fork falhou\n");
-        }
-        else if (ArvEsq == 0){
-			printf("n = %d        C[%d, %d]\n", nivel, getpid(), getppid());
-            gera_arvore_branch(nivel, altura, pid_root);
-        }
-        else {
-            wait(&ArvEsq);
-            ArvDir = fork();
-            if (ArvDir < 0){
-                printf("Erro.\n");
-            }
-
-            if (ArvDir == 0){
-    			printf("n = %d        C[%d, %d]\n", nivel, getpid(), getppid());
-                gera_arvore_branch(nivel, altura, pid_root);
-            }
-            else {
-				wait(&ArvDir);
-				printf("             T[%d, %d]\n", getpid(), getppid());
-			 	if (ROOT != getpid())				
-					exit(1); 
-                else if (getpid() == pid_root)
-                    printf("----- Fim criacao do Branch -----\n");
-                    return;
-            }
-        }
-    }
-    else {
-        printf("             T[%d, %d]\n", getpid(), getppid());
-        exit(0);
-    }
-}
-
-
-// void gera_arvore_livre(int nivel, int altura)
-// {
-//     pid_t ArvEsq, ArvDir;
-
-//     if (nivel == 0){
-//         printf("--------------- Livre ---------------\n");
-//         printf("n = 0        PID: %d (root)\n", getpid());
-//     }
-
-//     nivel++;
-//     if (nivel <= altura){
-//         ArvEsq = fork();
-//         if (ArvEsq < 0){
-// 			fprintf(stderr, "fork falhou\n");
-//         }
-//         else if (ArvEsq == 0){
-// 			printf("n = %d        C[%d, %d]\n", nivel, getpid(), getppid());
-//             gera_arvore_branch(nivel, altura);
-//         }
-//         else {
-//             ArvDir = fork();
-//             if (ArvDir < 0){
-//                 printf("Erro.\n");
-//             }
-
-//             if (ArvDir == 0){
-//     			printf("n = %d        C[%d, %d]\n", nivel, getpid(), getppid());
-//                 gera_arvore_branch(nivel, altura);
-//             }
-//             else {
-//                 wait(&ArvEsq);
-// 				wait(&ArvDir);
-// 				printf("             T[%d, %d]\n", getpid(), getppid());
-// 			 	if (ROOT != getpid())				
-// 					exit(1);
-// 				else
-// 					return;
-//             }
-//         }
-//     }
-//     else {
-//         printf("             T[%d, %d]\n", getpid(), getppid());
-//     }
-    
-
-// }
